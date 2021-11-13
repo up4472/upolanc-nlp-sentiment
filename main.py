f932882f7cf99f79c1f0014680f3035c116443e7
up4472 <74776855+up4472@users.noreply.github.com>
2021-11-13 22:30:02 +0100

    Commit #002

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from nltk.corpus import stopwords
+from logging import FileHandler
+from logging import Formatter
+from logging import Logger
 
 from main_classificaiton import main_classification
 from src.dataset import compute_polarity
@@ -19,28 +22,37 @@ pandas.set_option('display.max_rows', 1000)
 pandas.set_option('display.max_columns', 1000)
 pandas.set_option('display.width', 1000)
 
-def create_logger (filename : str = None, level : int = logging.DEBUG) -> None :
-	logging.basicConfig(
-		filename = filename,
-		format = '%(asctime)s - %(name)s : %(levelname)s : %(message)s',
-		level = level
-	)
+def create_logger (filename : str = None, level : int = logging.DEBUG) -> Logger :
+	logger = logging.getLogger()
+	handler = FileHandler(filename = filename, mode = 'w', encoding = 'utf-8')
 
-def download_resources () -> None :
-	logging.info('Downloading [stopwords] from [nltk]...')
+	handler.setFormatter(fmt = Formatter('%(asctime)s - %(name)s : %(levelname)s : %(message)s'))
+	logger.setLevel(level = level)
+	logger.addHandler(handler)
+
+	return logger
+	# 	filename = filename,
+	# 	filemode = 'w',
+	# 	format = '%(asctime)s - %(name)s : %(levelname)s : %(message)s',
+	# 	encoding = 'UTF-8',
+	# 	level = level
+	# )
+
+def download_resources (logger : Logger) -> None :
+	logger.info('Downloading [stopwords] from [nltk]...')
 	nltk.download('stopwords', quiet = True)
 
-	logging.info('Downloading [names] from [nltk]...')
+	logger.info('Downloading [names] from [nltk]...')
 	nltk.download('names', quiet = True)
 
-	logging.info('Downloading [wordnet] from [nltk]...')
+	logger.info('Downloading [wordnet] from [nltk]...')
 	nltk.download('wordnet', quiet = True)
 
-	logging.info('Downloading [vader_lexicon] from [nltk]...')
+	logger.info('Downloading [vader_lexicon] from [nltk]...')
 	nltk.download('vader_lexicon', quiet = True)
 
-def main () -> None :
-	logging.info('Loading dataset and configuration files...')
+def main (logger : Logger) -> None :
+	logger.info('Loading dataset and configuration files...')
 
 	dataset = load_csv(filename = 'airlines.csv')
 	config = load_json(filename = 'airlines.json')
@@ -48,9 +60,9 @@ def main () -> None :
 	dataset = dataset[config['columns']]
 	dataset = dataset.rename(columns = {'airline_sentiment' : 'target'})
 
-	logging.info('Checking for any missing values in the dataset...\n')
-	logging.debug('Missing values:\n' + str(dataset.isnull().sum()) + '\n')
-	logging.debug('Dataset header:\n' + str(dataset.head()) + '\n')
+	logger.info('Checking for any missing values in the dataset...\n')
+	logger.debug('Missing values:\n' + str(dataset.isnull().sum()) + '\n')
+	logger.debug('Dataset header:\n' + str(dataset.head()) + '\n')
 
 	en_stopwords = stopwords.words('english')
 	en_punct = string.punctuation
@@ -58,33 +70,34 @@ def main () -> None :
 	distribution = dataset['target'].value_counts().to_frame(name = 'count')
 	distribution['frequency'] = distribution['count'] / distribution['count'].sum()
 
-	logging.debug('Sentiment distribution:\n' + str(distribution) + '\n')
+	logger.debug('Sentiment distribution:\n' + str(distribution) + '\n')
 
-	logging.info('Cleaning dataset text...')
+	logger.info('Cleaning dataset text...')
 	dataset = clean_text(dataset = dataset, column = 'text', stopwords = en_stopwords, punct = en_punct)
 
-	logging.info('Calculating polarity scores...')
+	logger.info('Calculating polarity scores...')
 	dataset = compute_polarity(dataset = dataset, column = 'text')
 
-	logging.info('Tokenizing dataset text...')
+	logger.info('Tokenizing dataset text...')
 	dataset = tokenize_text(dataset = dataset, column = 'text')
 
-	logging.info('Stemming dataset text...')
+	logger.info('Stemming dataset text...')
 	dataset = stem_tokens(dataset = dataset, column = 'text')
 
-	logging.info('Lemmatizing dataset text...')
+	logger.info('Lemmatizing dataset text...')
 	dataset = lemmatize_tokens(dataset = dataset, column = 'text')
 
-	logging.debug('Dataset header:\n' + str(dataset.head()) + '\n')
+	logger.debug('Dataset header:\n' + str(dataset.head()) + '\n')
 
 	pos_words, neg_words = sentimental_words(dataset = dataset, column = 'text', target = 'target', top = 100)
 
-	logging.debug('Positive words : ' + str(pos_words))
-	logging.debug('Negative words : ' + str(neg_words) + '\n')
+	logger.debug('Positive words : ' + str(pos_words))
+	logger.debug('Negative words : ' + str(neg_words) + '\n')
 
-	main_classification(dataset = dataset, pos_words = pos_words, neg_words = neg_words)
+	main_classification(dataset = dataset, pos_words = pos_words, neg_words = neg_words, logger = logger)
 
 if __name__ == '__main__' :
-	create_logger(filename = None, level = logging.DEBUG)
-	download_resources()
-	main()
+	file_logger = create_logger(filename = 'output.log', level = logging.DEBUG)
+
+	download_resources(logger = file_logger)
+	main(logger = file_logger)
--- main_classificaiton.py
+++ main_classificaiton.py
@@ -1,4 +1,5 @@
 from pandas import DataFrame
+from logging import Logger
 
 from src.dataset import compute_pos_neg
 from src.dataset import encode_target
@@ -7,77 +8,92 @@ from src.feature import vader_features_ext
 from src.models import evaluate_classification
 from src.models import train_model
 
-import logging
 import numpy
 
-def __report (name : str, results : dict) -> None :
+def __report (name : str, results : dict, logger : Logger) -> None :
 	mean_acc = numpy.mean(results['accuracy_score'])
 	std_acc = numpy.std(results['accuracy_score'])
 
 	mean_bs = numpy.mean(results['brier_score'])
 	std_bs = numpy.std(results['brier_score'])
 
-	logging.info(f'[{name:24}] Accuracy score : {mean_acc:.5f} \u00B1 {std_acc:.5f}')
-	logging.info(f'[{name:24}] Brier score    : {mean_bs:.5f} \u00B1 {std_bs:.5f}\n')
+	logger.info(f'[{name:24}] Accuracy score : {mean_acc:.5f} \u00B1 {std_acc:.5f}')
+	logger.info(f'[{name:24}] Brier score    : {mean_bs:.5f} \u00B1 {std_bs:.5f}\n')
 
-def __class_majority (dataset : DataFrame) -> None :
+def __class_majority (dataset : DataFrame, logger : Logger) -> None :
 	majority = numpy.empty(shape = (len(dataset), 1))
 	majority.fill(dataset['target'].mode()[0])
 
 	probabilty = numpy.empty(shape = (len(dataset), 3))
 	probabilty[:, dataset['target'].mode()[0]] = 1
 
-	result = evaluate_classification(
+	results = evaluate_classification(
 		ytrue = dataset['target'].to_numpy(),
 		ypred = majority,
 		yprob = probabilty
 	)
 
-	__report('Majority classifier', result)
+	__report(
+		name = f'Majority classifier',
+		results = results,
+		logger = logger
+	)
 
-def __class_vader (dataset : DataFrame) -> None :
-	result = evaluate_classification(
+def __class_vader (dataset : DataFrame, logger : Logger) -> None :
+	results = evaluate_classification(
 		ytrue = dataset['target'].to_numpy(),
 		ypred = dataset['vader_prediction'].to_numpy()
 	)
 
-	__report('Vader if-else', result)
+	__report(
+		name = f'Vader features [if-else]',
+		results = results,
+		logger = logger
+	)
 
-def __class_vader_ml (dataset : DataFrame, model_name : str) -> None :
+def __class_vader_ml (dataset : DataFrame, model_name : str, logger : Logger) -> None :
 	xdata, ydata = vader_features(dataset = dataset)
 
 	results = train_model(xdata = xdata, ydata = ydata, model_name = model_name, k_fold = 10)
 
-	__report(f'Vader Features [{model_name}]', results)
+	__report(
+		name = f'Vader Features [{model_name}]',
+		results = results,
+		logger = logger
+	)
 
-def __class_vader_ml_ext (dataset : DataFrame, model_name : str) -> None :
+def __class_vader_ml_ext (dataset : DataFrame, model_name : str, logger : Logger) -> None :
 	xdata, ydata = vader_features_ext(dataset = dataset)
 
 	results = train_model(xdata = xdata, ydata = ydata, model_name = model_name, k_fold = 10)
 
-	__report(f'Vader Features Ext [{model_name}]', results)
+	__report(
+		name = f'Vader Features Ext [{model_name}]',
+		results = results,
+		logger = logger
+	)
 
-def main_classification (dataset : DataFrame, pos_words : set, neg_words : set) -> None :
-	logging.info('Creating classification target...')
+def main_classification (dataset : DataFrame, pos_words : set, neg_words : set, logger : Logger) -> None :
+	logger.info('Creating classification target...')
 	dataset, encoder = encode_target(dataset = dataset, target = 'target', column = 'vader_prediction')
 
-	logging.info('Calculating number of positive and negative words....\n')
+	logger.info('Calculating number of positive and negative words....\n')
 	dataset = compute_pos_neg(dataset = dataset, column = 'text', pos_words = pos_words, neg_words = neg_words)
 
-	logging.info('Adding a constant to all columns that have negative values....\n')
+	logger.info('Adding a constant to all columns that have negative values....\n')
 	dataset['vader_compound'] = dataset['vader_compound'].apply(lambda x : 1 + x)
 
-	logging.debug('Dataset header:\n' + str(dataset.head()) + '\n')
+	logger.debug('Dataset header:\n' + str(dataset.head()) + '\n')
 
 	# MAJORITY VOTING
-	__class_majority(dataset = dataset)
+	__class_majority(dataset = dataset, logger = logger)
 
 	# VADER
-	__class_vader(dataset = dataset)
+	__class_vader(dataset = dataset, logger = logger)
 
 	for name in ['MNB', 'GNB', 'DT', 'KNN', 'RF'] :
-		__class_vader_ml(dataset = dataset, model_name = name)
-		__class_vader_ml_ext(dataset = dataset, model_name = name)
+		__class_vader_ml(dataset = dataset, model_name = name, logger = logger)
+		__class_vader_ml_ext(dataset = dataset, model_name = name, logger = logger)
 
 	# BERT
 	...
